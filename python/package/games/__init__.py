cd941949ecccb0e33f98ba3430b753c95f0d2b46
duncan <duncan.kampert@surf.nl>
2023-10-09 12:26:02 +0200

    Removed unnecessary files, AGZ moved to https://github.com/Fjf/AlphaGoZero-MPI

--- python/package/games/__init__.py
+++
@@ -1,6 +0,0 @@
-import games.connect4.connect4
-import games.connect4.connect4_nn
-import games.hive.hive
-import games.hive.hive_nn
-import games.tictactoe.tictactoe
-import games.tictactoe.tictactoe_nn
--- python/package/mpi_packet.py
+++
@@ -1,9 +0,0 @@
-class MPIPacketState:
-    PROCESS = 0
-    TERMINATE = 1
-
-
-class MPIPacket:
-    def __init__(self, state: MPIPacketState, data):
-        self.state = state
-        self.data = data
